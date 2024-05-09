0cefddf54e87d863c37e901c7a1203bfcf25df02
proft <proftua@gmail.com>
2024-05-09 17:09:33 +0300

    Initial Commit

---
+++ ShopSwiftUI/ShopSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopSwiftUIApp.swift
+//  ShopSwiftUI
+//
+//  Created by Ivan Morgun on 09.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct ShopSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
